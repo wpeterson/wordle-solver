fee6ab1fbfb1ea07931d219e428b88c284f90d27
Winfield Peterson <winfield@aldenscientific.com>
2022-01-24 15:07:05 -0500

    Adds first version of WordList API

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source "https://rubygems.org"
 ruby '2.7.5'
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'algorithms'
+
 group :test do
   gem 'rspec'
 end
---
+++ lib/word_list.rb
@@ -0,0 +1,51 @@
+require 'algorithms'
+
+class WordList
+  attr_reader :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def self.from_file(path)
+    new(File.open(path) { |file| file.each.map {|line| line.chomp } })
+  end
+
+  def is_valid?(word)
+    prefix_trie.has_key?(word)
+  end
+
+  def each
+    words.each
+  end
+
+  def size
+    words.size
+  end
+
+  def without_letters(letters)
+    letters_regex = regex_for_letters(letters)
+    self.class.new(words.select {|word| !word.match(letters_regex) })
+  end
+
+  def with_letters(letters)
+    letters_regex = regex_for_letters(letters)
+    self.class.new(words.select {|word| word.match(letters_regex) })
+  end
+
+  def filter(pattern)
+    self.class.new(prefix_trie.wildcard(pattern))
+  end
+
+private
+
+  def regex_for_letters(letters)
+    letters = letters.chars if letters.is_a?(String)
+
+    /(#{letters.join('|')})/
+  end
+
+  def prefix_trie
+    @prefix_trie ||= Containers::Trie.new.tap { |trie| words.each { |word| trie.push(word, word) } }
+  end
+end
